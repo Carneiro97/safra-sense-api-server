1fcb6de42608f5e81c90f58b46e6efe9eee4d12b
Daniel <daniel.alexandre97@hotmail.com>
2020-09-12 15:51:05 -0300

    fix: package.json mistake on 'start' tag

--- server.js
+++ server.js
@@ -6,8 +6,3 @@ const port = process.env.PORT || 3030;
 const server = http.createServer(app);
 
 server.listen(port);
-
-var express = require('express');
-var app     = express();
-
-app.set('port', (process.env.PORT || 5000));
