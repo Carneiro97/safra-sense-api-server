68711dd47da5d47560cb98bd36cd0df423c2f018
Daniel <daniel.alexandre97@hotmail.com>
2020-09-12 19:44:45 -0300

    config: changing connection string for umbler bd

--- api/routes/sentimento.js
+++ api/routes/sentimento.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const Empresa = require('../models/Empresa');
+const Empresa = require('../models/empresa');
 
 const SentimentoEnum = require('../enums/SentimentoEnum');
 
