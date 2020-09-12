ad58ac7b9dc605307cae95618661d8c60263c3e6
Daniel <daniel.alexandre97@hotmail.com>
2020-09-12 15:05:18 -0300

    test: testing heroku server deployment

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
-const http = require('http');
-const app = require('./app');
+// const http = require('http');
+// const app = require('./app');
 
-const port = process.env.PORT || 3030;
-console.log(process.env.PORT);
-console.log(process.env.port);
-console.log(port);
+// const port = process.env.PORT || 3030;
 
-const server = http.createServer(app);
+// const server = http.createServer(app);
 
-server.listen(port);
+// server.listen(port);
+
+var express = require('express');
+var app     = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
